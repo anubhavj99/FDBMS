b01ae14cbb2a564bf8ba78fd52b8628cd5d16081
Anubhav Jaiswal <anubhav@Anubhavs-MacBook-Air.local>
2018-01-12 18:48:03 +0530

    database made from input file

--- program2.py
+++ program2.py
@@ -7,27 +7,14 @@ def getTableMetaData(metaFile):
 	return x;
 
 def readInputFile(inputFile, dataTypes):
-	# x = inputFile.read();
-	# x = x.split('\n');
-	# for i in range(len(x)):
-	# 	x[i] = x[i].split(' ');
-	# for dt in dataTypes:
-	# 	if dt[0] not in x[0]:
-	# 		print('error');
-	# 		return False;
-	# for i in range(1, len(x)):
-	# 	for j in range(len(dataTypes)):
-	# 		if dataTypes[j][1] == 'I' and len(x[i][j]) < int(dataTypes[j][2]):
-	# 			for k in x[i][j]:
-	# 				if ord(k) not in range(48, 58):
-	# 					return false;
-	# 				else:
-	# 					print(k, end="I ");
-	# 		elif dataTypes[j][1] == 'C' and len(x[i][j]) < int(dataTypes[j][2]):
 	totalLength = 0;
 	for dt in dataTypes:
 		totalLength += int(dt[2]);
 	x = inputFile.read();
+
+	if len(x) % totalLength !=0:
+		raise Exception('File unexpected');
+
 	x = [x[i:i+totalLength] for i in range(0, len(x), totalLength)];
 	fullX = [];
 	for data in x:
@@ -37,8 +24,18 @@ def readInputFile(inputFile, dataTypes):
 			lensum += int(dt[2]);
 			temp.append(data[lensum-int(dt[2]):lensum]);
 		fullX.append(temp);
-	print(fullX)
 
+	for i in range(len(fullX)):
+		for j in range(len(fullX[i])):
+			if dataTypes[j][1] == 'C':
+				fullX[i][j] = fullX[i][j].strip();
+			elif dataTypes[j][1] == 'I':
+				fullX[i][j] = int(fullX[i][j]);
+			elif dataTypes[j][1] == 'P':
+				fullX[i][j] = float(fullX[i][j]);	
+	
+	print(fullX);
+	return fullX;
 				
 
 
@@ -48,11 +45,10 @@ def readInputFile(inputFile, dataTypes):
 if __name__ == '__main__':
 	metaFile = open('metadata','r');
 	dataTypes = getTableMetaData(metaFile);
-	# print(dataTypes);
 	dataFile = open('datafile','r+');
-	# getTableData(dataFile, dataTypes);
 	inputFile = open('inputfile', 'r');
-	readInputFile(inputFile, dataTypes);
-	# putFromInputToDataFile(inputFile, dataFile, dataTypes);
+
+	dataBase = readInputFile(inputFile, dataTypes);
+	
 	metaFile.close();
 	dataFile.close();
